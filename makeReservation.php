b02bf2c90cac8e6bbed74bfc133569e6a4f53d26
InaVuzelova <ina.vuzelova@gmail.com>
2024-04-23 18:16:30 +0300

    Reservation form

--- makeReservation.php
+++ makeReservation.php
@@ -7,7 +7,12 @@
 	<title></title>
 </head>
 <body>
-<?php include 'create.php'; ?>
+<?php include 'create.php'; 
+
+$sqlSelectRoomTypes = "SELECT roomType FROM roomtypes";
+$resultRoomTypes = mysqli_query($dbConn, $sqlSelectRoomTypes);
+
+?>
 
 <h2>MAKE A RESERVATION: </h2>
     <form method="post" action="makeReservation.php" class="custom">
@@ -17,18 +22,23 @@
                   	for ($i = 1; $i <= 30; $i++) {
         		  		echo "<option value='$i'>$i</option>";
     			  	}
-    			  ?> <br>
-        Full Name: <input type="text" name="fullName" required><br>
-        Number of guests: <select name="people">
+    			  ?> </select> <br>
+        Full Name: <input type="text" name="clientName" required><br>
+        Phone Number <input type="tel" name="clientPhone"><br>
+        Number Of Guests: <select name="people">
                   		  <?php
                   		  	for ($i = 1; $i <= 5; $i++) {
         		  				echo "<option value='$i'>$i</option>";
     			  		  	}
-    			  		  ?> <br>
-    	Room type: 
-        <input type="submit" name='submit' value="Log In">
+    			  		  ?> </select> <br>
+    	Room Type: <select name="roomType">
+                  <?php while ($row = mysqli_fetch_array($resultRoomTypes)):;?>
+                  <option value="<?php echo $row[0];?>"> <?php echo $row[0];?> </option>
+                  <?php endwhile; ?>
+                  </select><br>
+        <input type="submit" name='submit' value="Submit">
         &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-        Don't have an account? <a href="signup.php" class="sign-up-link">Sign up </a>
+        <!--Don't have an account? <a href="signup.php" class="sign-up-link">Sign up </a> -->
     </form>
 <br><a href="startPage.php">
         <img src="arrow.png" width="50" height="35" align="left">
