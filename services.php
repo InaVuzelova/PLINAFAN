bd3fd5d8c963e856568d3e9de07ba0c20a03f51f
Stefan Spirov <st.spiroff@gmail.com>
2024-05-11 22:27:55 +0300

    Updated Services page

--- services.php
+++ services.php
@@ -6,14 +6,41 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Services</title>
 </head>
+<style>
+	h1 {
+  text-shadow: 5px 2px 20px #050300;
+}
+p {
+  text-shadow: 5px 5px 15px #050300;
+}
+</style>
 <body>
 <?php 
 include 'create.php';
 include 'menu.php';
 ?>
+<div style="display:flex; font-size:150%;">
+<h1 style="color:#f0e0bf; text-align:center; margin-left: 100px;"<i><b>Конна езда</b></i></h1>
+<h1 style="color:#f0e0bf; text-align:center; margin-left: 500px;"<i><b>Гимнастика за духа и тялото</b></i></h1>
+</div>
 
+<div style="display:flex">
+<p style="color:white; font-size:160%; width:500px; text-align:justify; margin-left: 10px;"><b>В близост до PLINAFAN се намира конна база предлагаща туристическа езда в гората, а за опитни ездачи има и препятствия за прескачане и упражнения по обездка. 
 
-<br><a href="startPage.php">
+<br><br>Професионални треньори ще се погрижат да ви въведат в магията на този невероятен спорт, съчетаващ, най-различни възможности, както и такива за лечение, чрез хипотерапия.
+
+</b></p>
+<p style="color:white; font-size:160%; width:500px; text-align:justify; margin-left:320px;"><b>Аеробика, йога, стречинг, аква гимнастика и много други.<br>
+
+В PLINAFAN гостите ни могат да се присъединят към разнообразни активности като професионални тренировки от стречинг до Kangoo Jumps.<br><br>
+
+Също така всеки ден предлагаме аква гимнастика или аква забавление в басейните на много хотели.<br>
+
+За програмата просто попитайте някой от нашите спортни аниматори.</b></p>
+</div>
+<div style="display:flex">
+</div>
+<a href="startPage.php">
         <img src="arrow.png" width="50" height="35" align="left">
 </body>
 </html>
